76dcf1f39c0e07d58744ab0e28515b60a654aac6
Ajay kumar Pondugala <ajaykumar.pondugala@meraki.com>
2026-09-23 02:11:20 +0530

    Hide project verification dates

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -52,7 +52,6 @@ export default async function ProjectPage({ params }: { params: Promise<{ slug: 
             repositoryUrl={project.repositoryUrl}
             status={project.status}
           />
-          <p>Evidence checked {project.lastVerified}</p>
         </div>
       </header>
 
